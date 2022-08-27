8e9422d90689c795f6758125342aed3b71bde750
Sebastian <sebastian.velo@mercadolibre.com>
2022-08-26 22:16:14 -0300

    add ShowExplorePageBodyModel

---
+++ functions/src/app/model/body-page/explore/ShowExplorePageBodyModel.ts
@@ -0,0 +1,27 @@
+import { LanguageParams } from "tmdb-js/lib/api/common/params/CommonParams";
+import { TVShowsResponse } from "tmdb-js/lib/api/common/response/CommonResponse";
+import { ExplorePageBodyModel } from "../../../../shared/model/pages/explore/ExplorePageModel";
+import TMDB from "../../../../tmdb/TMDB";
+import SectionHelper from "../../../helper/section/SectionHelper";
+
+const ShowExplorePageBodyModel = async (query?: LanguageParams): Promise<ExplorePageBodyModel> => {
+    const onTheAir: TVShowsResponse = await TMDB.tvShow.getOnTheAir(query);
+    const topRated: TVShowsResponse = await TMDB.tvShow.getTopRatedShows(query);
+    const popular: TVShowsResponse = await TMDB.tvShow.getPopularShows(query);
+    const airingToday: TVShowsResponse = await TMDB.tvShow.getAiringToday(query);
+    const dailyTrending: TVShowsResponse = await TMDB.trending.getTVShows("day");
+    const weeklyTrending: TVShowsResponse = await TMDB.trending.getTVShows("week");
+
+    return {
+        sections: SectionHelper.show.getExplore({
+            onTheAir,
+            topRated,
+            popular,
+            airingToday,
+            dailyTrending,
+            weeklyTrending
+        })
+    };
+};
+
+export default ShowExplorePageBodyModel;
--- functions/src/app/service/ExplorePageService.ts
+++ functions/src/app/service/ExplorePageService.ts
@@ -5,6 +5,7 @@ import * as TitleHelper from "../helper/title/TitleHelper";
 import { DetailPageBodyModel } from "../../shared/model/pages/detail/DetailPageModel";
 import ExplorePageModel from "../../shared/model/pages/explore/ExplorePageModel";
 import MovieExplorePageBodyModel from "../model/body-page/explore/MovieExplorePageBodyModel";
+import ShowExplorePageBodyModel from "../model/body-page/explore/ShowExplorePageBodyModel";
 
 class ExplorePageService {
   public static async getMovie(query?: LanguageParams): Promise<ExplorePageModel> {
@@ -17,7 +18,7 @@ class ExplorePageService {
   }
 
   public static async getShow(query?: LanguageParams): Promise<ExplorePageModel> {
-    const body: DetailPageBodyModel = await BodyPageHelper.show.getExplore(query);
+    const body: DetailPageBodyModel = await ShowExplorePageBodyModel(query);
     return {
       title: TitleHelper.show.getExplore(),
       searchbar: SearchBarHelper.show.getSearchbar(),
