a195e422cfdb9e19753e9c8cd57a1e179cbc876e
Sebastian <sebastian.velo@mercadolibre.com>
2025-05-12 20:30:38 -0300

    feat: Modularize ShowDetailPage by splitting into body, header, sections, and search components

---
+++ functions/src/api/pages/detail/page/ShowDetailPage.search.ts
@@ -0,0 +1,10 @@
+import SearchBarModel from "shared/model/components/SearchBarModel";
+import PageRoute from "shared/routes/PageRoute";
+
+const SearchBar = (): SearchBarModel => ({
+  placeholder: `Search shows...`,
+  path: PageRoute.SHOW_SEARCH
+});
+
+
+export default SearchBar;
